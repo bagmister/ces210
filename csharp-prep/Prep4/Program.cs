553f1716a4f4e0cd8a9879a1e84dc0c0ae26488e
kevin bagley <kevin.bagley@progleasing.com>
2024-04-24 22:58:16 -0600

    week 1 prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numberList = new List<int>();
+        var numberToAdd = -1;
+        while(numberToAdd != 0){
+            Console.WriteLine("Enter a number");
+            numberToAdd = int.Parse(Console.ReadLine());
+            if (numberToAdd != 0)
+            {
+                numberList.Add(numberToAdd);
+            }
+
+        }
+        var numberSum = numberList.Sum();
+        var numberAverage = numberList.Average();
+        var numbersMax = numberList.Max();
+        Console.WriteLine($"The sum is : {numberSum} ");
+        Console.WriteLine($"The average is : {numberAverage} ");
+        Console.WriteLine($"The largest number is : {numbersMax} ");
+        
     }
 }
