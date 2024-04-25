22862deb9581574b57ab79e91a763610da3e581e
kevin bagley <kevin.bagley@progleasing.com>
2024-04-24 22:02:11 -0600

    prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Greetings, Enter your grade to see what the letter representation is. Example 80.");
+        int grade = int.Parse(Console.ReadLine());
+        Console.WriteLine($"You entered {grade}.");
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your leter grade is an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your leter grade is an B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your leter grade is an C");
+        }
+        else if(grade >= 60)
+        {
+            Console.WriteLine("Your leter grade is an D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("Your leter grade is an F");
+        }
     }
 }
