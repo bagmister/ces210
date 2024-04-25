f9e2498122f79aa7f58ee62f754263f943a4fabe
kevin bagley <kevin.bagley@progleasing.com>
2024-04-24 23:38:18 -0600

    week 1 prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        
+        var userName = PromptUserName();
+        var userNumber = PromptUserNumber();
+        var squareNumber= SquareNumber(userNumber);
+        
+        DisplayResult(squareNumber, userName);
+
+        static void DisplayMessage(){
+            Console.WriteLine("Welcome to the program!!");
+        }
+        static string PromptUserName(){
+            Console.WriteLine("Please enter your name: ");
+            var userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber(){
+            Console.WriteLine("Please enter your favorite number: ");
+            var userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+        static int SquareNumber(int number){
+            var numberSqured = number * number;
+            return numberSqured;
+        }
+        static void DisplayResult(int squaredNumber, string userName){
+            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        }
     }
+    
 }
