2e402edb8936885b23b30ae484bbb3ea3c034e25
kevin bagley <kevin.bagley@progleasing.com>
2024-04-24 22:36:32 -0600

    prep 3 week 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, 100);
+        Console.WriteLine($"the magic number is {magicNumber}");
+        int guess = 0;
+        
+        while(guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess for the magic number?");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < magicNumber)
+                Console.WriteLine("Higher");
+            else if (guess > magicNumber){
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } ;
+
+
+
     }
 }
