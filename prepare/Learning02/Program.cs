a5de9569d5f3ebfe1446344700337cab61a75e71
kevin bagley <kevin.bagley@progleasing.com>
2024-04-30 22:43:45 -0600

    week 2 prep1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job();
+        var job2 = new Job();
+        var resume = new Resume();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume._name = "Nate Bedopadope";
+
+    resume.DisplayResume();
+
     }
 }
+
